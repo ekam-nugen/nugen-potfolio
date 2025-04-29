15eef476a4b8d598fd63cdd26aaadcc8a0c75c17
Harmandeep <harmannugen@gmail.com>
2025-04-29 17:49:29 +0530

    fix icon height

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Loader from "@/src/components/common/Loader";
 import TestimonialCard from "@/src/components/TestimonialCard";
--- src/components/Home/platform.tsx
+++ src/components/Home/platform.tsx
@@ -108,8 +108,8 @@ export default function PlatformSolutions() {
                   <Image
                     src={partner.icon}
                     alt={partner.title}
-                    width={100}
-                    height={100}
+                    width={60}
+                    height={60}
                     className="object-contain"
                   />
                   <span className="text-base font-semibold text-gray-900">
